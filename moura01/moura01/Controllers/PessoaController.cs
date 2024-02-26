5d0193ce2fc1c8a69cb5d2df5f7d5b49b0965ccc
Arthur Paschoalini de Azevedo <arthurp.wild@gmail.com>
2024-02-26 17:12:50 -0300

    first commit

---
+++ moura01/moura01/Controllers/PessoaController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using moura01.models;
+
+namespace moura01.Controllers
+{
+    [ApiController]
+    [Route("v1/[controller]")]
+    public class PessoaController : ControllerBase
+    {
+        private static List<Pessoa> pessoas = [];
+
+    
+        [HttpGet(Name = "pessoas")]
+        public IEnumerable<Pessoa> GetAll()
+        {
+            
+
+            return pessoas ;
+
+              
+        }
+
+        [HttpPost(Name = "pessoa")]
+        public IActionResult Post(Pessoa pessoa)
+        {
+            if (pessoa == null)
+            {
+                return BadRequest("Pessoa invalida");
+            } 
+            pessoas.Add (pessoa);
+            return Ok(pessoa);
+        }
+    }
+}
---
+++ moura01/moura01/models/Pessoa.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace moura01.models
+{
+    [Table(name:"pessoas")]
+    public class Pessoa
+    {
+        [Key]
+        [Column( name:"codigo" )]
+        [Required]
+        public int Codigo { get; set; }
+
+        [Required]
+        [Column(name: "nome")]
+        [StringLength(50)]
+        public string? Nome { get; set; }
+
+        [Required]
+        [Column(name: "datanascimento")]
+        public DateTime DataNascimento { get; set; }
+
+        [Required]
+        [Column(name:"inativo")]
+        public bool Inativo { get; set; }
+
+        [Required]
+        [Column(name:"nacionalidade")]
+        public int Nacionalidade { get; set; }
+
+        [Column(name:"rg")]
+        [StringLength(20)]
+        public string? RG { get; set; }
+
+        [Column(name:"passaporte")]
+        [StringLength(20)]
+        public string? Passaporte { get; set; }
+    }
+}
